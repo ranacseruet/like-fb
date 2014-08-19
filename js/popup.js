90c2a07c06757e988c0a898141285f51a88f4ed8
nila <rana@EnvyM6-Rana>
2014-08-19 01:10:13 -0400

    small changes on js file.

--- js/popup.js
+++ js/popup.js
@@ -16,5 +16,6 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toGMTString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+    var path    = "path=/";
+    document.cookie = cname + "=" + cvalue + "; " + expires + "; " + path;
 }
